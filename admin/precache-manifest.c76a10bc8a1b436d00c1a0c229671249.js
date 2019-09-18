692e1539228cba26f31c2cf7d3615a0e6ea282a2
root <root@203899.local>
2019-09-18 12:08:28 +0300

    добавил заготовку для админки на реакте

---
+++ admin/precache-manifest.c76a10bc8a1b436d00c1a0c229671249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ad002b1c26131a402c77cd57bb394c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae9f286c0e6d603a8fb0",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "86b9124cdf7de20cebc2",
+    "url": "/static/js/2.7d4ab0b3.chunk.js"
+  },
+  {
+    "revision": "ae9f286c0e6d603a8fb0",
+    "url": "/static/js/main.4043c78d.chunk.js"
+  },
+  {
+    "revision": "9c8f7cdd6e1149e5c2a0",
+    "url": "/static/js/runtime~main.2c40bb78.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
