9979c6922f699b4f418a8316381ceb197d69cb0a
root <root@203899.local>
2019-09-21 19:51:57 +0300

     Правки по админке

--- frontendAdminDEV/admin_form/src/index.js
+++ frontendAdminDEV/admin_form/src/index.js
@@ -41,5 +41,44 @@ class AdminPanel extends React.Component {
   }
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<AdminPanel />, rootElement);
+class Authorization extends React.Component{
+    onClick(){
+
+       if( document.querySelector('#log').value === 'admin' && 
+           document.querySelector('#pass').value === 'qwe') {
+               ReactDOM.render(<Response isSuccess={true} />,  document.getElementById('root'));
+           }else{
+               alert('Неверный логин или пароль');
+               ReactDOM.render(<Response isSuccess={false} />,  document.getElementById('root'));
+           }
+
+    }
+    componentDidMount() {
+
+    }
+    render(){
+        return (
+             <div className='Authorization'>
+                <h1>Авторизация пользователя</h1>
+                    <div className='line'>
+                        <div className='row'>Логин:</div> <div className='row'><input id='log'/></div>
+                    </div>
+                    <div className='line'>
+                        <div className='row'> Пароль:</div>  <div className='row'><input id='pass'/></div>
+                    </div>
+                    <span onClick={ this.onClick }>
+                         <div className='sbm'>Залогиниться</div>
+                    </span>
+             </div>
+        )
+    }
+}
+
+function Response(props) {
+
+    const isSuccess = props.isSuccess;
+    return isSuccess ? <AdminPanel/> : <Authorization/>
+
+  }
+
+ReactDOM.render(<Response isSuccess={false} />,  document.getElementById('root')); 
